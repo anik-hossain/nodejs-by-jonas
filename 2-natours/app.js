44f95d8e7152eab093065a16ebd2e2d19d36a3cf
Anik Hossain <anik.wdev@gmail.com>
2022-04-23 21:30:41 +0600

    get tours from the json file

--- 2-natours/app.js
+++ 2-natours/app.js
@@ -2,8 +2,26 @@
  * Dependencies
  */
 const express = require('express');
+const fs = require('fs');
 
+/**
+ * Create Application
+ */
 const app = express();
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'Success',
+        result: tours.length,
+        data: {
+            tours,
+        },
+    });
+});
 
 const port = 4000;
 
