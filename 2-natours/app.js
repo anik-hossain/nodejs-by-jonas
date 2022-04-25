40411bf1bcbf87f3caf2ca4cb8736ab44d9ca698
Anik Hossain <anik.wdev@gmail.com>
2022-04-25 15:38:32 +0600

    set static folder

--- 2-natours/app.js
+++ 2-natours/app.js
@@ -14,6 +14,7 @@ const app = express();
 // Middleware
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 // Routing
 app.use('/api/v1/tours/', tourRoutes);
