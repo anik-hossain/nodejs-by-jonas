e39da535edcef7f0e29f84b2b4c19cf53458c2bc
Anik Hossain <anik.wdev@gmail.com>
2022-04-26 19:50:40 +0600

    implement pagination

--- 2-natours/controllers/tourController.js
+++ 2-natours/controllers/tourController.js
@@ -25,12 +25,24 @@ exports.getAllTours = async (req, res) => {
             query = query.sort('createdAt');
         }
 
-        // 3 - Field limited
+        // 3 - Fields limited
         if (req.query.fields) {
             const fields = req.query.fields.split(',').join(' ');
             query = query.select(fields);
         } else {
             query = query.select('-__v');
+        }
+
+        // 4 - Pagination
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 100;
+        const skip = (page - 1) * limit;
+
+        query = query.skip(skip).limit(limit);
+
+        if (req.query.page) {
+            const numTours = await Tour.countDocuments();
+            if (skip >= numTours) throw new Error('Page does not exist');
         }
 
         // Execute query
