e38f8865e16fc0ed433f61274b7868e2db25bf77
Anik Hossain <anik.wdev@gmail.com>
2022-04-27 18:05:40 +0600

    custom validators

--- 2-natours/models/tourModel.js
+++ 2-natours/models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
     {
@@ -16,6 +17,7 @@ const tourSchema = new mongoose.Schema(
                 10,
                 'A tour name must have at less or equal then 10 characters',
             ],
+            // validate: [validator.isAlpham ,'Tour name must only contain characters'],
         },
         slug: String,
         duration: {
@@ -48,7 +50,17 @@ const tourSchema = new mongoose.Schema(
             type: Number,
             required: [true, 'A tour must have a price'],
         },
-        priceDiscount: Number,
+        priceDiscount: {
+            type: Number,
+            validate: {
+                validator(value) {
+                    // this only point to current doc on NEW Document
+                    return value < this.price;
+                },
+                message:
+                    'Discount price ({VALUE}) should be below regular price',
+            },
+        },
         summary: {
             type: String,
             trim: true,
