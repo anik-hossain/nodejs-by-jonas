f7b2f3ac55c26a8682de5361ed76006ead5f4c5e
Anik Hossain <anik.wdev@gmail.com>
2022-04-28 13:09:34 +0600

    errors during development and production

--- 2-natours/controllers/errorController.js
+++ 2-natours/controllers/errorController.js
@@ -1,9 +1,38 @@
+const sendErrorDev = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+        error: err,
+        stack: err.stack,
+    });
+};
+
+const sendErrorProduction = (err, res) => {
+    // Operational, trusted error: send to client
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message,
+        });
+    }
+    // Programming or other unknown error: don't leak error details in client
+    else {
+        console.error('Error 😡', err);
+
+        res.status(500).json({
+            status: 'Server side error',
+            message: 'Something went wrong in server',
+        });
+    }
+};
+
 module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'Server side error occurred';
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message,
-    });
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorDev(err, res);
+    } else if (process.env.NODE_ENV === 'production') {
+        sendErrorProduction(err, res);
+    }
 };
