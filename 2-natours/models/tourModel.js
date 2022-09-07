384706c8060810b730103ba2265c1f827e73dec9
Anik Hossain <anik.wdev@gmail.com>
2022-09-07 16:24:18 +0600

    refector Tour Model

--- 2-natours/models/tourModel.js
+++ 2-natours/models/tourModel.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
     {
@@ -17,7 +16,7 @@ const tourSchema = new mongoose.Schema(
                 10,
                 'A tour name must have at less or equal then 10 characters',
             ],
-            // validate: [validator.isAlpham ,'Tour name must only contain characters'],
+            // validate: [validator.isAlpha ,'Tour name must only contain characters'],
         },
         slug: String,
         duration: {
@@ -85,6 +84,39 @@ const tourSchema = new mongoose.Schema(
             type: Boolean,
             default: false,
         },
+        startLocation: {
+            // GeoJSON
+            type: {
+                type: String,
+                default: 'Point',
+                enum: ['Point'],
+            },
+            coordinates: [Number],
+            address: String,
+            description: String,
+        },
+        locations: [
+            {
+                type: {
+                    type: String,
+                    default: 'Point',
+                    enum: ['Point'],
+                },
+                coordinates: [Number],
+                address: String,
+                description: String,
+                day: Number,
+            },
+        ],
+        // Child Referencing
+        guides: [
+            {
+                type: mongoose.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+        // Tours Guides Embedding Models
+        // guides: Array,
     },
     {
         toJSON: { virtuals: true },
@@ -102,32 +134,39 @@ tourSchema.pre('save', function (next) {
     next();
 });
 
-// tourSchema.pre('save', function (next) {
-//     console.log('Will save document...');
-//     next();
-// });
+//  Populate Tour Guide
+tourSchema.pre(/^find/, function (next) {
+    this.populate({
+        path: 'guides',
+        select: '-__v -passwordChangedAt',
+    });
+    next();
+});
 
-// tourSchema.post('save', function (doc, next) {
-//     console.log(doc);
+// Tours Guides Embedding Models
+// tourSchema.pre('save', async function (next) {
+//     const guidesPromises = this.guides.map(
+//         async (id) => await User.findById(id)
+//     );
+//     this.guides = await Promise.all(guidesPromises);
 //     next();
 // });
 
 // Query middleware
 tourSchema.pre(/^find/, function (next) {
     this.find({ secretTour: { $ne: true } });
-    // this.start = Date.now();
+    this.start = Date.now();
     next();
 });
-
-// tourSchema.post(/^find/, function (docs, next) {
-//     console.log('Query took ', Date.now() - this.start, ' millisoconds');
-//     console.log(docs);
-//     next();
-// });
 
 // Aggregation middleware
 tourSchema.pre('aggregate', function (next) {
     this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+    next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+    console.log('Query took ', Date.now() - this.start, ' milliseconds');
     next();
 });
 
