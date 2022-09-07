06bda555d4aa41e201e651ee1587838b3fcc2f4b
Anik Hossain <anik.wdev@gmail.com>
2022-09-07 20:43:05 +0600

    simple nested routes

--- 2-natours/routes/tourRoutes.js
+++ 2-natours/routes/tourRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { protect, restrictTo } = require('../controllers/authController');
+const { createReview } = require('../controllers/reviewController');
+
 const {
     createTour,
     deleteTour,
@@ -22,5 +24,9 @@ router
     .get(getTour)
     .patch(updateTour)
     .delete(protect, restrictTo('admin', 'lead-guide'), deleteTour);
+
+router
+    .route('/:tourId/reviews')
+    .post(protect, restrictTo('user'), createReview);
 
 module.exports = router;
