75e1a200598acf43d4c94635af3deed55db8213a
Anik Hossain <anik.wdev@gmail.com>
2022-09-08 16:20:04 +0600

    gain read performance with indexes

--- 2-natours/controllers/handlerFactory.js
+++ 2-natours/controllers/handlerFactory.js
@@ -49,6 +49,7 @@ exports.getAll = (Model) =>
             .limitFields()
             .pagination();
 
+        // const doc = await features.query.explain();
         const doc = await features.query;
 
         // Response to client
