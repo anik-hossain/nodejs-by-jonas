1ee23c2f136029ee7099f2636be99b1e46090443
Anik Hossain <anik.wdev@gmail.com>
2022-09-08 13:02:00 +0600

    added missing authentication & authorization into routes

--- 2-natours/routes/userRoutes.js
+++ 2-natours/routes/userRoutes.js
@@ -6,6 +6,7 @@ const {
     resetPassword,
     updatePassword,
     protect,
+    restrictTo,
 } = require('../controllers/authController');
 const {
     createUser,
@@ -22,16 +23,19 @@ const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
-
-router.get('/me', protect, getMe, getUser);
 router.post('/forgot', forgotPassword);
 router.patch('/password-reset/:token', resetPassword);
-router.patch('/update-password', protect, updatePassword);
-router.patch('/update-profile', protect, update_profile);
-router.delete('/delete-profile', protect, delete_profile);
 
+// Protect all routes after this middleware
+router.use(protect);
+router.patch('/update-password', updatePassword);
+router.patch('/update-profile', update_profile);
+router.delete('/delete-profile', delete_profile);
+router.get('/me', getMe, getUser);
+
+// Restrict all routes after this middleware
+router.use(restrictTo('admin'));
 router.route('/').get(getAllUsers).post(createUser);
-
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
 module.exports = router;
