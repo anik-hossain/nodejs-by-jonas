896bb830857a2d02c6b85f7e27380baffac1dc5b
Anik Hossain <anik.wdev@gmail.com>
2022-09-09 14:55:28 +0600

    home page created

--- 2-natours/controllers/viewsController.js
+++ 2-natours/controllers/viewsController.js
@@ -1,8 +1,17 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+    // 1. Get Tour Data From Collection
+    const tours = await Tour.find();
+
+    // 2. Build Template
+    // 3. Render That Template Using Tour Data From 1
     res.status(200).render('overview', {
         title: 'All Tours',
+        tours,
     });
-};
+});
 
 exports.getTour = (req, res) => {
     res.status(200).render('tour', {
