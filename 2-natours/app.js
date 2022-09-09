efdd34abae98fd29c644c638df28fe2bb8a78c34
Anik Hossain <anik.wdev@gmail.com>
2022-09-09 12:00:06 +0600

    seting up project structure

--- 2-natours/app.js
+++ 2-natours/app.js
@@ -16,6 +16,7 @@ const AppError = require('./utils/AppError');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 /**
  * Create Application
@@ -70,10 +71,7 @@ app.use(
 );
 
 // Routing
-app.get('/', (req, res) => {
-    res.status(200).render('base');
-});
-
+app.use('/', viewRouter);
 app.use('/api/v1/tours/', tourRouter);
 app.use('/api/v1/users/', userRouter);
 app.use('/api/v1/reviews/', reviewRouter);
