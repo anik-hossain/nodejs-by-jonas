f44b6fec78530d33454bcafe870a6ea09b28e29c
Anik Hossain <anik.wdev@gmail.com>
2022-09-09 21:50:18 +0600

    tour details complete🔥

--- 2-natours/controllers/viewsController.js
+++ 2-natours/controllers/viewsController.js
@@ -5,16 +5,25 @@ exports.getOverview = catchAsync(async (req, res, next) => {
     // 1. Get Tour Data From Collection
     const tours = await Tour.find();
 
-    // 2. Build Template
-    // 3. Render That Template Using Tour Data From 1
     res.status(200).render('overview', {
         title: 'All Tours',
         tours,
     });
 });
 
-exports.getTour = (req, res) => {
-    res.status(200).render('tour', {
-        title: 'The Forest Hiker',
+exports.getTour = catchAsync(async (req, res) => {
+    const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+        path: 'reviews',
+        fields: 'review rating user',
     });
-};
+
+    res.status(200)
+        .set(
+            'Content-Security-Policy',
+            "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
+        )
+        .render('tour', {
+            title: `${tour.name} Tour`,
+            tour,
+        });
+});
